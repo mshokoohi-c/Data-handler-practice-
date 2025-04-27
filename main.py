db885b5a2f9cdc6eed63545e6683ec61551e18d9
mshokoohi-c <mshokoohi004@gmail.com>
2025-04-26 22:21:48 -0700

    changed main, and added sample data

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print('hello world')
+import csv
+
+data = []
+
+#anything that needs to use the csv file needs to live indented under with because it closes the file after the with block ends
+with open('sampledata.csv', mode= 'r') as file: 
+
+    reader = csv.reader(file)
+    
+
+    for row in reader: 
+        data.append(row)
+
+        
+
+print(data[2])
+hex= data[2][0].split()
+print(hex[2])
+#print(data[2][1])
